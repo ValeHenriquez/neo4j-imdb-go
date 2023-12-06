8e817527655cfe4aa699bc675204a70025f69440
ValeHenriquez <vale.belen.vhf@gmail.com>
2023-12-05 21:41:54 -0400

    refactor fillDB function to use goroutines

--- tmdb_api/database.api.go
+++ tmdb_api/database.api.go
@@ -1,42 +1,91 @@
 package api
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/ValeHenriquez/neo4j-imdb-go/database"
 	"github.com/ValeHenriquez/neo4j-imdb-go/models"
 	"github.com/ValeHenriquez/neo4j-imdb-go/utils"
 )
 
 func fillDB() error {
-	idsPopular, err := getPopularMoviesIds()
+	var wg sync.WaitGroup
+	wg.Add(2) 
+
+	go func() {
+		defer wg.Done()
+		if err := fillDBCategory(getPopularMoviesIds, fillDBWithRecommendationsMovie); err != nil {
+			fmt.Println("Error filling movies:", err)
+		}
+		fmt.Println("MOVIES CARGADAS")
+	}()
+
+	go func() {
+		defer wg.Done()
+		if err := fillDBCategory(getPopularSeriesIds, fillDBWithRecommendationsSerie); err != nil {
+			fmt.Println("Error filling series:", err)
+		}
+		fmt.Println("SERIES CARGADAS")
+	}()
+
+	wg.Wait() 
+	return nil
+}
+
+func fillDBCategory(getIDs func() ([]int64, error), fillFunc func(int64) error) error {
+	ids, err := getIDs()
 	if err != nil {
 		return err
 	}
 
-	for _, id := range idsPopular {
-		if err := fillDBWithRecommendations(id); err != nil {
+	for _, id := range ids {
+		if err := fillFunc(id); err != nil {
 			return err
 		}
 	}
 
+	return nil
+}
+
+func fillDBWithRecommendationsSerie(serieID int64) error {
+	mainSerie, mainActors, mainGenres, err := getSeriesDetailsById(serieID)
+	if err != nil {
+		return err
+	}
+
+	if err := fillDBCategory(func() ([]int64, error) { return getSerieRecommendationsIds(serieID) }, func(id int64) error {
+		serie, actors, genres, err := getSeriesDetailsById(id)
+		if err != nil {
+			return err
+		}
+		if err := fillDBSerie(serie, actors, genres); err != nil {
+			return err
+		}
+		return database.CreateBiRelation(mainSerie, serie, utils.RECOMMENDS)
+	}); err != nil {
+		return err
+	}
+
+	return fillDBSerie(mainSerie, mainActors, mainGenres)
+}
+
+func fillDBSerie(serie models.Serie, actors []models.Actor, genres []models.Genre) error {
+	if err := createCategoryAndRelations(&serie, actors, genres, utils.ACTED_IN, utils.CATEGORIZED); err != nil {
+		return err
+	}
 	return nil
 }
 
 func fillDBMovie(movie models.Movie, actors []models.Actor, genres []models.Genre) error {
-	if err := createMovieAndRelations(movie, actors, utils.ACTED_IN); err != nil {
+	if err := createCategoryAndRelations(&movie, actors, genres, utils.ACTED_IN, utils.CATEGORIZED); err != nil {
 		return err
 	}
-
-	for _, genre := range genres {
-		if err := createGenreAndRelation(movie, genre, utils.CATEGORIZED); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
 
-func createMovieAndRelations(movie models.Movie, actors []models.Actor, relationType string) error {
-	if err := database.Create(&movie); err != nil {
+func createCategoryAndRelations(category interface{}, actors []models.Actor, genres []models.Genre, actorRelation, genreRelation string) error {
+	if err := database.Create(category); err != nil {
 		return err
 	}
 
@@ -44,7 +93,16 @@ func createMovieAndRelations(movie models.Movie, actors []models.Actor, relation
 		if err := database.Create(&actor); err != nil {
 			return err
 		}
-		if err := database.CreateRelation(&movie, &actor, relationType); err != nil {
+		if err := database.CreateRelation(category, &actor, actorRelation); err != nil {
+			return err
+		}
+	}
+
+	for _, genre := range genres {
+		if err := database.Create(&genre); err != nil {
+			return err
+		}
+		if err := database.CreateRelation(category, &genre, genreRelation); err != nil {
 			return err
 		}
 	}
@@ -52,29 +110,13 @@ func createMovieAndRelations(movie models.Movie, actors []models.Actor, relation
 	return nil
 }
 
-func createGenreAndRelation(movie models.Movie, genre models.Genre, relationType string) error {
-	if err := database.Create(&genre); err != nil {
-		return err
-	}
-	return database.CreateRelation(&movie, &genre, relationType)
-}
-
-func fillDBWithRecommendations(movieID int64) error {
+func fillDBWithRecommendationsMovie(movieID int64) error {
 	mainMovie, mainActors, mainGenres, err := getMovieDetailsById(movieID)
 	if err != nil {
 		return err
 	}
 
-	if err := fillDBMovie(mainMovie, mainActors, mainGenres); err != nil {
-		return err
-	}
-
-	idsRecommendations, err := getMovieRecommendationsIds(movieID)
-	if err != nil {
-		return err
-	}
-
-	for _, id := range idsRecommendations {
+	if err := fillDBCategory(func() ([]int64, error) { return getMovieRecommendationsIds(movieID) }, func(id int64) error {
 		movie, actors, genres, err := getMovieDetailsById(id)
 		if err != nil {
 			return err
@@ -82,11 +124,10 @@ func fillDBWithRecommendations(movieID int64) error {
 		if err := fillDBMovie(movie, actors, genres); err != nil {
 			return err
 		}
-
-		if err := database.CreateBiRelation(mainMovie, movie, utils.RECOMMENDS); err != nil {
-			return err
-		}
+		return database.CreateBiRelation(mainMovie, movie, utils.RECOMMENDS)
+	}); err != nil {
+		return err
 	}
 
-	return nil
+	return fillDBMovie(mainMovie, mainActors, mainGenres)
 }
